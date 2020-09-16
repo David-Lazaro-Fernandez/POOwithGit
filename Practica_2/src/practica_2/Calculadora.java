05198a2bf59dad44659ba3795cdd09e0cf3c883d
David Lazaro Fernandez <davidlazaro20@hotmail.com>
2020-09-15 21:44:53 -0500

    Documentación de la practica 2
    Se describio linea por linea que hacia el programa realizado durante la practica 2

--- Practica_2/src/practica_2/Calculadora.java
+++ Practica_2/src/practica_2/Calculadora.java
@@ -10,20 +10,27 @@ package practica_2;
  * @author David Lazaro
  */
 public class Calculadora {
+    /**
+     *  
+        c->Variable que almacena el resultado de las operaciones
+        d->Variable que almacena la division flotante de a2 / b2
+        a2-> Variable de tipo float que almacena el valor de a 
+        b2-> Variable de tipo float que almacena el valor de b
+     */
     int c;
     float d,a2,b2;
     public Calculadora(int a,int b){
         
-        c=a+b;
-        System.out.println("Suma:"+c);
-        c=a-b;
-        System.out.println("Resta:"+c);
-        c=a*b;
-        System.out.println("Multiplicacion:"+c);
-        a2=a;
-        b2=b;
-        d=a2/b2;
-        System.out.println("Division:"+d);
+        c=a+b; //Suma de a y b
+        System.out.println("Suma:"+c); //Impresion en pantalla del valor de c
+        c=a-b; //Resta de a y b
+        System.out.println("Resta:"+c);//Impresion en pantalla del valor de c
+        c=a*b; //Multiplicacion de a y b
+        System.out.println("Multiplicacion:"+c);//Impresion en pantalla del valor de c
+        a2=a; //a2 almacena el valor de a
+        b2=b; //b2 almacena el valor de b
+        d=a2/b2; //Division con decimal de a2 y b2
+        System.out.println("Division:"+d); //Impresion en pantalla del valor de d
     }
 }
 
--- Practica_2/src/practica_2/Practica_2.java
+++ Practica_2/src/practica_2/Practica_2.java
@@ -4,19 +4,20 @@
  * and open the template in the editor.
  */
 package practica_2;
-import java.util.Scanner;
+import java.util.Scanner; //Importamos la libreria Scanner 
 /**
  *
  * @author David Lazaro
  */
 public class Practica_2 {
     public static void main(String[] args) {
-        int a,b;
-        Scanner input = new Scanner(System.in);
-        System.out.println("Ingrese 2 numeros a operar:"); 
-        a = input.nextInt();
-        b = input.nextInt();
-        Calculadora cs = new Calculadora(a,b); 
+        int a,b;                                            //Declaracion de los 2 datos a ingresar
+        Scanner input = new Scanner(System.in);             //Inicializacion de la variable tipo Scanner
+        System.out.println("Ingrese 2 numeros a operar:");  
+        a = input.nextInt();                                //Lectura del primer dato                                    
+        b = input.nextInt();                                //Lectura del segundo dato
+        Calculadora cs = new Calculadora(a,b);              //Llamado de la funcion calculadora
+        
         
     }
     
